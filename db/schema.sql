759b815c17a83e471ce61d7b893b7f1be30b9dfe
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-11-06 13:06:31 -0800

    add roles

--- db/schema.sql
+++ db/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE department (
 CREATE TABLE role ( 
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salaray DECIMAL NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INTEGER,
     INDEX dep_ind (department_id),
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE SET NULL
