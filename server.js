a9ff8109bf25deefe6c63ba6a4595b0338526f30
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-11-06 09:05:31 -0800

    initial prompt

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //Import MySQL
-const mysql = require('mysql');
+const mysql = require('mysql2');
 //Import inquirer
 const inquirer = require('inquirer');
 //Imort Console Table
@@ -7,9 +7,48 @@ const cTable = require('console.table');
 //Setup dotenv
 require('dotenv').config()
 
-//Connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: process.env.MYSQL_PASSWORD
-})
+// connection to database
+const connection = require('connection.js')
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('connected as id' + connection.threadId);
+    afterConnection();
+});
+
+//Welcome screen after connection
+afterConnection = () => {
+    console.log("***********************************")
+    console.log("                                   ")
+    console.log("         EMPLOYEE TRACKER          ")
+    console.log("        by: Robert Arnold          ")
+    console.log("                                   ")
+    console.log("***********************************")
+    promptUser();
+}    
+
+//Inquirer prompt for starting
+const promptUser = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Update an employee role',
+                'Update an employee manager',
+                'View employees by deartment',
+                'Delete a department',
+                'Delete a role',
+                'Delete an employee',
+                'View dearment budgets',
+                'No Action'
+            ]
+        }
+    ])
+}
