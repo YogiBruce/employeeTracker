f33e185781132b030315ec1befc4ca05e0e8b724
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-11-06 09:22:11 -0800

    conditionals to start selection functions

--- server.js
+++ server.js
@@ -40,9 +40,10 @@ const promptUser = () => {
                 'View all employees',
                 'Add a department',
                 'Add a role',
+                'Add an employee',
                 'Update an employee role',
                 'Update an employee manager',
-                'View employees by deartment',
+                'View employees by department',
                 'Delete a department',
                 'Delete a role',
                 'Delete an employee',
@@ -51,4 +52,65 @@ const promptUser = () => {
             ]
         }
     ])
-}
+
+    .then((answers) => {
+        const { choices } = answers;
+        
+        if (choices === 'View all departments'){
+            showDepartments();
+        };
+    
+        if (choices === 'View all roles'){
+            showRoles();
+        };
+    
+        if (choices === 'View all employees'){
+            showEmployees();
+        };
+    
+        if (choices === 'Add a department'){
+            addDepartment();
+        };
+    
+        if (choices === 'Add a role'){
+            addRole();
+        };
+    
+        if (choices === 'Add an employee'){
+            addEmployee();
+        };
+    
+        if (choices === 'Update an employee role'){
+            updateEmployee();
+        };
+    
+        if (choices === 'Update an employee manager'){
+            updateManager();
+        };
+    
+        if (choices === 'View employees by department'){
+            employeeDepartment();
+        };
+    
+        if (choices === 'Delete a department'){
+            deleteDepartment();
+        };
+    
+        if (choices === 'Delete a role'){
+            deleteRole();
+        };
+    
+        if (choices === 'Delete an employee'){
+            deleteEmployee();
+        };
+    
+        if (choices === 'View department budgets'){
+            viewBudget();
+        };
+    
+        if (choices === 'No action'){
+            connection.end();
+        };
+    });
+};
+
